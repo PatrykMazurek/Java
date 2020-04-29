703fb34cb8076ae2f70c35daf2079084be5f9492
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2020-04-29 20:11:29 +0200

    Patryk Mazurek
    Uzupełnienie metod do obsługi klienta FTP

--- Server/AppFTPClient.java
+++ Server/AppFTPClient.java
@@ -72,6 +72,7 @@ public class AppFTPClient {
                         case "4":
                             System.out.println("Proszę o podanie pliku do usunięcia:");
                             String delFile = input.nextLine();
+							RemoveFile(delFile)
                             break;
                         case "5":
                             System.out.println("Proszę podać plik do zmiany nazwy:");
@@ -103,4 +104,142 @@ public class AppFTPClient {
             System.out.println(host);
         }
     }
+	
+	    public boolean ConnectToFTP(){
+        session = null;
+        try{
+            session = jsch.getSession(ftpData.GetLogin(), host, 22);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword(ftpData.GetPassword());
+            session.connect(10000);
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            home = channelSftp.getHome() + "/public_html/";
+            channelSftp.cd(home);
+            return true;
+        } catch (JSchException e) {
+            e.printStackTrace();
+            return false;
+        } catch (SftpException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void DisconnectFTP(){
+        if (session.isConnected()){
+            channelSftp.exit();
+            channel.disconnect();
+            session.disconnect();
+            System.out.println("Zakończyłem prace");
+        }
+    }
+
+    public void ShowAllFile(String location){
+        try{
+            String tempLocation;
+            if (location.isEmpty()){
+                tempLocation = channelSftp.pwd();
+            }else{
+                tempLocation = channelSftp.pwd() + "/" + location;
+            }
+
+            Vector listAllFile = channelSftp.ls(tempLocation);
+            for (int i = 0; i< listAllFile.size(); i++){
+                ChannelSftp.LsEntry file = (ChannelSftp.LsEntry) listAllFile.get(i);
+                if(!file.getFilename().endsWith(".")){
+                    System.out.println(file.getFilename() + " " + file.getAttrs().getSize());
+                }
+            }
+        } catch (SftpException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void DownloadFile(String fileLocation, String destLocation){
+        try{
+            String tempLocation = null;
+            if(fileLocation.contains("/")){
+                if (channelSftp.ls(channelSftp.pwd() + "/" + fileLocation).size() > 0){
+                    tempLocation = channelSftp.pwd() + "/" + fileLocation;
+                }
+            }else{
+                tempLocation = channelSftp.pwd()+"/"+fileLocation;
+            }
+            channelSftp.get(tempLocation, destLocation);
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Plik nie istnieje " + fileLocation);
+        }
+    }
+
+    public void UpLoadFile(String fileLocation, String destLoccation){
+        try{
+            String[] tempLocation;
+            if(destLoccation.contains("/")){
+                tempLocation = destLoccation.split("/");
+                for (String s : tempLocation){
+                    if(s.length() > 0 || s.contains(".")){
+                        try{
+                            channelSftp.cd(s);
+                        } catch (SftpException e) {
+                            System.out.println("Kataloh nie istnieje");
+                            System.out.println("Czy utworzyć katalog: (T/N) " + s);
+                            Scanner in = new Scanner(System.in);
+                            if(in.nextLine().equals("T")){
+                                channelSftp.mkdir(s);
+                                channelSftp.cd(s);
+                            }else{
+                                System.out.println("Przerwano wysłanie pliku");
+                                break;
+                            }
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            channelSftp.put(fileLocation, channelSftp.pwd());
+        } catch (SftpException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void RemoveFile(String fileName){
+        try{
+            if(channelSftp.ls(channelSftp.pwd() + "/" + fileName).size() > 0){
+                Scanner in = new Scanner(System.in);
+                System.out.println("Czy chcesz usuną plik: (T/N) ?");
+                System.out.println(channelSftp.pwd() + "/" + fileName);
+                if(in.nextLine().equals("T")){
+                    channelSftp.rm(channelSftp.pwd() + "/" + fileName);
+                    System.out.println("Usunięto plik");
+                }
+            }
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Plik nie istnieje");
+        }
+    }
+
+    public void RenameFile(String location, String oldFile, String newFile){
+        try{
+            channelSftp.rename(location+"/"+oldFile, location+"/"+newFile);
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Wystąpił problem ze zmianą nazwy pliku");
+        }
+
+    }
+
+    public void MoveFromCatalog(String folder){
+        try{
+            channelSftp.cd(folder);
+        } catch (SftpException e) {
+            //e.printStackTrace();
+            System.out.println("Nie ma takiego katalogu: " + folder);
+        }
+    }
+	
+	
 }
